3931da7e1cefdd7409b047bf466c91ce12c6a733
admin <C:\Users\admin\AppData\Roaming\The Bat!>
2026-01-15 12:42:31 +0300

    маршрутор

---
+++ config/urls.py
@@ -0,0 +1,25 @@
+"""
+URL Configuration for novosty_crm project.
+Routes are organized by responsibility following SOLID principles.
+"""
+
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic import TemplateView
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    # Admin panel
+    path('admin/', admin.site.urls),
+    
+    # API endpoints (delegated to contacts app)
+    path('api/', include('apps.contacts.urls')),
+    
+    # Frontend (serves static HTML)
+    path('', TemplateView.as_view(template_name='index.html'), name='home'),
+]
+
+# Serve static files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
